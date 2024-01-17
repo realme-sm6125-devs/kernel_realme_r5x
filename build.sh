Amritorock/biofrost-kernel-realme-trinket

build.sh: update
- change build host to dizzy
- drop GAS and use distro's cross compiler for aosp clang
- tidy up some sections

Signed-off-by: mcdofrenchfreis <xyzevan@androidist.net>

build.sh
@@ -5,7 +5,8 @@
 
 # Dependency preparation
 make clean mrproper
-git clean -fdx; git reset --hard
+git clean -fdx
+git reset --hard
 rm -rf AnyKernel/{*.zip,*.img,Image.gz-dtb}
 git clone --depth=1 https://github.com/mcdofrenchfreis/AnyKernel3.git -b r5x AnyKernel
 
@@ -13,7 +14,6 @@ git clone --depth=1 https://github.com/mcdofrenchfreis/AnyKernel3.git -b r5x Any
 DATE=$(TZ=Asia/Singapore date +"%a %b %d %r %Z %Y")
 BUILD_START=$(date +"%s")
 TCDIR=/home/biofrost/Development/Compiler/AndroidClang
-GASDIR=/home/biofrost/Development/Compiler/PrebuiltGAS
 DTBO=$(pwd)/out/arch/arm64/boot/dtbo.img
 IMAGE=$(pwd)/out/arch/arm64/boot/Image.gz-dtb
 
@@ -31,7 +31,7 @@ export REPO_URL="https://github.com/mcdofrenchfreis/biofrost-kernel-realme-trink
 export COMPILER_NAME="$(${TCDIR}/bin/clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g' -e 's/[[:space:]]*$//')"
 export LINKER_NAME="$("${TCDIR}"/bin/ld.lld --version | head -n 1 | sed 's/(compatible with [^)]*)//' | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g' -e 's/[[:space:]]*$//')"
 export KBUILD_BUILD_USER=xevan
-export KBUILD_BUILD_HOST=R32.GTS-t
+export KBUILD_BUILD_HOST=dizzy
 export DEVICE="Realme 5 Series"
 export CODENAME="realme_trinket"
 export BUILD_TYPE="Bleeding-Edge"
@@ -42,9 +42,9 @@ CI_ID="-1001736789494"
 BOT_ID="5129489057:AAF5o-JfQ1iAUp9Min7Jcr9sHPjTpCaIlA8"
 
 sendinfo() {
-  kernel_version=$(make kernelversion 2>/dev/null)
-  commit_details=$(git log --pretty=format:'%s' -1)
-  message="<b>Laboratory Machine || CI Build Triggered</b>
+    kernel_version=$(make kernelversion 2>/dev/null)
+    commit_details=$(git log --pretty=format:'%s' -1)
+    message="<b>Laboratory Machine || CI Build Triggered</b>
 <b>Docker: </b><code>${DISTRO}</code>
 <b>Build Date: </b><code>${DATE}</code>
 <b>Device: </b><code>${DEVICE} (${CODENAME})</code>
@@ -62,6 +62,7 @@ sendinfo() {
         -d "parse_mode=html" \
         -d text="$message"
 }
+
 function push() {
     ZIP=$(find AnyKernel -maxdepth 1 -name '*.zip' -type f -printf '%T@ %p\n' | sort -n | tail -1 | awk '{print $2}')
     MD5CHECKSUM=$(md5sum "$ZIP" | cut -d' ' -f1)
@@ -71,34 +72,37 @@ function push() {
         -F "parse_mode=html" \
         -F caption="Build took $(($DIFF / 60)) minutes and $(($DIFF % 60)) seconds. | <b>Compiled with ${COMPILER_NAME}</b> | <b>MD5 Checksum: </b><code>${MD5CHECKSUM}</code>."
 }
+
 function finerr() {
     curl -s -X POST "https://api.telegram.org/bot${BOT_ID}/sendMessage" \
         -d chat_id="$CI_ID" \
         -d "disable_web_page_preview=true" \
         -d "parse_mode=html" \
         -d text="Compilation failed, please check build logs for errors."
-  exit 1
+    exit 1
 }
+
 function compile() {
     make O=out ARCH=arm64 biofrost_defconfig
-    export PATH="${TCDIR}/bin:${GASDIR}:${PATH}"
+    export PATH="${TCDIR}/bin:${PATH}"
     export CROSS_COMPILE=aarch64-linux-gnu-
     export CLANG_TRIPLE=aarch64-linux-gnu-
     export CROSS_COMPILE_ARM32=arm-linux-gnueabi-
-    make -j$(nproc --all) O=out LLVM=1 LLVM-IAS=1 \
+    make -j$(nproc --all) O=out LLVM=1 LLVM-IAS=1
 
     if ! [ -a "$IMAGE" ] || ! [ -a "$DTBO" ]; then
         finerr
-        exit 1
     fi
     cp out/arch/arm64/boot/Image.gz-dtb AnyKernel
     cp out/arch/arm64/boot/dtbo.img AnyKernel
 }
+
 function zipping() {
     cd AnyKernel || exit 1
     zip -r9 "$ZIP_NAME".zip . -x ".git*" -x "README.md" -x "LICENSE" -x "*.zip"
     cd ..
 }
+
 sendinfo
 compile
 zipping

realme-sm6125-devs/kernel_realme_r5x

build.sh
@@ -5,7 +5,8 @@
 
 # Dependency preparation
 make clean mrproper
-git clean -fdx; git reset --hard
+git clean -fdx
+git reset --hard
 rm -rf AnyKernel/{*.zip,*.img,Image.gz-dtb}
 git clone --depth=1 https://github.com/mcdofrenchfreis/AnyKernel3.git -b r5x AnyKernel
 
@@ -13,7 +14,6 @@ git clone --depth=1 https://github.com/mcdofrenchfreis/AnyKernel3.git -b r5x Any
 DATE=$(TZ=Asia/Singapore date +"%a %b %d %r %Z %Y")
 BUILD_START=$(date +"%s")
 TCDIR=/home/biofrost/Development/Compiler/AndroidClang
-GASDIR=/home/biofrost/Development/Compiler/PrebuiltGAS
 DTBO=$(pwd)/out/arch/arm64/boot/dtbo.img
 IMAGE=$(pwd)/out/arch/arm64/boot/Image.gz-dtb
 
@@ -31,7 +31,7 @@ export REPO_URL="https://github.com/mcdofrenchfreis/biofrost-kernel-realme-trink
 export COMPILER_NAME="$(${TCDIR}/bin/clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g' -e 's/[[:space:]]*$//')"
 export LINKER_NAME="$("${TCDIR}"/bin/ld.lld --version | head -n 1 | sed 's/(compatible with [^)]*)//' | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g' -e 's/[[:space:]]*$//')"
 export KBUILD_BUILD_USER=xevan
-export KBUILD_BUILD_HOST=R32.GTS-t
+export KBUILD_BUILD_HOST=nerdz
 export DEVICE="Realme 5 Series"
 export CODENAME="realme_trinket"
 export BUILD_TYPE="Bleeding-Edge"
@@ -42,9 +42,9 @@ CI_ID="-1001736789494"
 BOT_ID="5129489057:AAF5o-JfQ1iAUp9Min7Jcr9sHPjTpCaIlA8"
 
 sendinfo() {
-  kernel_version=$(make kernelversion 2>/dev/null)
-  commit_details=$(git log --pretty=format:'%s' -1)
-  message="<b>Laboratory Machine || CI Build Triggered</b>
+    kernel_version=$(make kernelversion 2>/dev/null)
+    commit_details=$(git log --pretty=format:'%s' -1)
+    message="<b>Laboratory Machine || CI Build Triggered</b>
 <b>Docker: </b><code>${DISTRO}</code>
 <b>Build Date: </b><code>${DATE}</code>
 <b>Device: </b><code>${DEVICE} (${CODENAME})</code>
@@ -62,6 +62,7 @@ sendinfo() {
         -d "parse_mode=html" \
         -d text="$message"
 }
+
 function push() {
     ZIP=$(find AnyKernel -maxdepth 1 -name '*.zip' -type f -printf '%T@ %p\n' | sort -n | tail -1 | awk '{print $2}')
     MD5CHECKSUM=$(md5sum "$ZIP" | cut -d' ' -f1)
@@ -71,34 +72,37 @@ function push() {
         -F "parse_mode=html" \
         -F caption="Build took $(($DIFF / 60)) minutes and $(($DIFF % 60)) seconds. | <b>Compiled with ${COMPILER_NAME}</b> | <b>MD5 Checksum: </b><code>${MD5CHECKSUM}</code>."
 }
+
 function finerr() {
     curl -s -X POST "https://api.telegram.org/bot${BOT_ID}/sendMessage" \
         -d chat_id="$CI_ID" \
         -d "disable_web_page_preview=true" \
         -d "parse_mode=html" \
         -d text="Compilation failed, please check build logs for errors."
-  exit 1
+    exit 1
 }
+
 function compile() {
     make O=out ARCH=arm64 biofrost_defconfig
-    export PATH="${TCDIR}/bin:${GASDIR}:${PATH}"
+    export PATH="${TCDIR}/bin:${PATH}"
     export CROSS_COMPILE=aarch64-linux-gnu-
     export CLANG_TRIPLE=aarch64-linux-gnu-
     export CROSS_COMPILE_ARM32=arm-linux-gnueabi-
-    make -j$(nproc --all) O=out LLVM=1 LLVM-IAS=1 \
+    make -j$(nproc --all) O=out LLVM=1 LLVM-IAS=1
 
     if ! [ -a "$IMAGE" ] || ! [ -a "$DTBO" ]; then
         finerr
-        exit 1
     fi
     cp out/arch/arm64/boot/Image.gz-dtb AnyKernel
     cp out/arch/arm64/boot/dtbo.img AnyKernel
 }
+
 function zipping() {
     cd AnyKernel || exit 1
     zip -r9 "$ZIP_NAME".zip . -x ".git*" -x "README.md" -x "LICENSE" -x "*.zip"
     cd ..
 }
+
 sendinfo
 compile
 zipping